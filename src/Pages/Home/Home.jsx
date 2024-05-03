bc5f1cb3948c76b7e189024eeeeb24e4351fe090
NadiasCodes <De.nadia@outlook.com>
2024-05-03 10:40:15 +0200

    added the logo, text and buttons for the hero and styled them in css

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.css";
 import Navbar from "../../Components/Navbar/Navbar";
 import HeroBanner from "../../assets/hero_banner.jpg";
 import HeroTtitle from "../../assets/hero_title.png";
+import PlayIcon from "../../assets/play_icon.png";
+import InfoIcon from "../../assets/info_icon.png";
 
 const Home = () => {
   return (
@@ -13,6 +15,15 @@ const Home = () => {
         <div className="hero-caption">
           <img src={HeroTtitle} alt="hero title" className="hero-caption-img" />
           <p>Tommy Shelby, a dangerous man, leads the Peaky Blinders, a gang based in Birmingham. Soon, Chester Campbell, an inspector, decides to nab him and put an end to the criminal activities.</p>
+          <div className="hero-buttons">
+            <button className="btn">
+              <img src={PlayIcon} alt="play button icon" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={InfoIcon} alt="info button icon" /> More Info
+            </button>
+          </div>
         </div>
       </div>
     </div>
