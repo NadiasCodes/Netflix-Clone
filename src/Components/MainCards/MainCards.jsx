3806209a82849f3a6c6e378709fa34f84ed4b839
NadiasCodes <De.nadia@outlook.com>
2024-05-04 13:48:03 +0200

    use the API from TMDB to display movies and tv-series

--- src/Components/MainCards/MainCards.jsx
+++ src/Components/MainCards/MainCards.jsx
@@ -1,24 +1,40 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./MainCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
-const MainCards = ({title, category}) => {
+const MainCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjhhNTdiNTI5MGFiYTBhNzMyYWQ2OGMxZjc2MTBkNyIsInN1YiI6IjY2MzYxOTQ2YzM5MjY2MDEyNjZlZDJjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MCf4Ss_cscsQpo3XNLiOsJ3GYOydmS8cMXWxEhpOxTE",
+    },
+  };
+
   const handleWhweel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   };
   useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=`, options)
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results))
+      .catch((err) => console.error(err));
+
     cardsRef.current.addEventListener("wheel", handleWhweel);
   }, []);
   return (
     <div className="maincards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt="card images" />
+              <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="card images" />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
